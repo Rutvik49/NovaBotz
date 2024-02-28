abe0d6c85c4ec8803169f5d90bf016cde18c5381
Rutvik <rutvikchaudhary0110@gmail.com>
2024-02-28 18:52:27 +0530

    sequelize and mysql database setted up.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,28 @@
-import connectDB from './db/index.js'
-import dotenv from 'dotenv'
-dotenv.config({
-    path:'/env'
+const express = require('express')
+const cors = require('cors')
+const db = require('./models')
+require('dotenv').config()
+
+const User = db.users
+
+const app = express()
+var corOption ={
+    origin: 'http://localhost:3000'
+}
+
+// middlewares
+app.use(cors(corOption))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// test api
+
+app.get('/',async (req,res)=>{
+    const data = await User.create({ firstName: "dax", lastName: "chaudhary" })
+    res.status(200).send(data)
 })
 
-connectDB()
+const port = process.env.PORT || 8000
+app.listen(port,()=>{
+    console.log(`app is running on port ${port}`);
+})
