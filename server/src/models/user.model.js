5b5cdbedbfa5a63da7baa2ae283d639d0215f612
Rutvik <rutvikchaudhary0110@gmail.com>
2024-03-05 20:51:45 +0530

    cloudnary and multer setup.

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,16 +1,39 @@
-module.exports=(sequelize,DataTypes)=>{
-    const User = sequelize.define('user',{
-        user_id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true 
-          },
-        firstName:{
-            type: DataTypes.STRING,
-        },
-        lastName:{
-            type: DataTypes.STRING,
-        }
-    })
-    return User
-}
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("user", {
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      trim: true,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      trim: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+      trim: true,
+    },
+    phoneNo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      trim: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    refreshToken: {
+      type: DataTypes.STRING,
+    },
+  });
+
+  return User;
+};
