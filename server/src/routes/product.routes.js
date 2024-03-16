be16311bb1ed0609e1664d49960a8fa9194ae4f7
Rutvik <rutvikchaudhary0110@gmail.com>
2024-03-16 18:57:06 +0530

    product operations implementation done.

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,5 +1,9 @@
 const Router = require("express");
-const { createProduct } = require("../controllers/product.controller.js");
+const {
+  createProduct,
+  getProduct,
+  removeProduct,
+} = require("../controllers/product.controller.js");
 const { verifyJWT } = require("../middlewares/authenticateUser.js");
 const { uploadOnMulter } = require("../middlewares/multer.js");
 const router = Router();
@@ -7,6 +11,7 @@ const router = Router();
 router
   .route("/add-product")
   .post(uploadOnMulter.array("avatar", 8), createProduct);
-// router.route("/get-product").get(verifyJWT, getAddress);
+router.route("/get-product").get(getProduct);
+router.route("/remove-product/:id").delete(removeProduct);
 
 module.exports = router;
