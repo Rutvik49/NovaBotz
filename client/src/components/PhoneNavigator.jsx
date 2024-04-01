ab353d1f6487b40c3f0ae3fa0ac535502bd46252
Rutvik <rutvikchaudhary0110@gmail.com>
2024-04-01 11:15:12 +0530

    phone Navigator fixes.

--- client/src/components/PhoneNavigator.jsx
+++ client/src/components/PhoneNavigator.jsx
@@ -11,52 +11,52 @@ function PhoneNavigator() {
             className={({ isActive }) =>
               `${
                 isActive
-                  ? "bg-lightBlue rounded-full w-1/5 flex flex-col py-2 m-auto"
-                  : "bg-bottomWhite w-1/5 flex flex-col py-2 m-auto"
+                  ? "bg-lightBlue rounded-full w-1/5 flex flex-col py-4 m-auto"
+                  : "bg-bottomWhite w-1/5 flex flex-col py-4 m-auto"
               }`
             }
           >
             <img src="images/home.svg" alt="" className="w-10 m-auto" />
-            <span className="m-auto text-mainBlue">Home</span>
+            {/* <span className="m-auto text-mainBlue">Home</span> */}
           </NavLink>
           <NavLink
             to="products"
             className={({ isActive }) =>
               `${
                 isActive
-                  ? "bg-lightBlue rounded-full w-1/5 flex flex-col py-2 m-auto"
-                  : "bg-bottomWhite w-1/5 flex flex-col py-2 m-auto"
+                  ? "bg-lightBlue rounded-full w-1/5 flex flex-col py-4 m-auto"
+                  : "bg-bottomWhite w-1/5 flex flex-col py-4 m-auto"
               }`
             }
           >
             <img src="images/product.svg" alt="" className="w-10 m-auto" />
-            <span className="m-auto text-mainBlue">Products</span>
+            {/* <span className="m-auto text-mainBlue ">Products</span> */}
           </NavLink>
           <NavLink
             to="blogs"
             className={({ isActive }) =>
               `${
                 isActive
-                  ? "bg-lightBlue rounded-full w-1/5 flex flex-col py-2 m-auto"
-                  : "bg-bottomWhite w-1/5 flex flex-col py-2 m-auto"
+                  ? "bg-lightBlue rounded-full w-1/5 flex flex-col py-4 m-auto"
+                  : "bg-bottomWhite w-1/5 flex flex-col py-4 m-auto"
               }`
             }
           >
             <img src="images/blog.svg" alt="" className="w-10 m-auto" />
-            <span className="m-auto text-mainBlue">Blogs</span>
+            {/* <span className="m-auto text-mainBlue">Blogs</span> */}
           </NavLink>
           <NavLink
             to="profile"
             className={({ isActive }) =>
               `${
                 isActive
-                  ? "bg-lightBlue rounded-full w-1/5 flex flex-col py-2 m-auto"
-                  : "bg-bottomWhite w-1/5 flex flex-col py-2 m-auto"
+                  ? "bg-lightBlue rounded-full w-1/5 flex flex-col py-4 m-auto"
+                  : "bg-bottomWhite w-1/5 flex flex-col py-4 m-auto"
               }`
             }
           >
             <img src="images/profile.svg" alt="" className="w-10 m-auto" />
-            <span className="m-auto text-mainBlue">Profile</span>
+            {/* <span className="m-auto text-mainBlue">Profile</span> */}
           </NavLink>
         </div>
       </nav>
